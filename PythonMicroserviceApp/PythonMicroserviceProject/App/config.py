f35371fa5baa3365eecff5d49e645d579144c8f7
Catrinescu Alexandru <76565929+Ka3nel@users.noreply.github.com>
2025-08-08 15:42:12 +0300

    Delete PythonMicroserviceApp directory

--- PythonMicroserviceApp/PythonMicroserviceProject/App/config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # Load variables from .env
-
-API_TOKEN = os.getenv("API_TOKEN", "default-token")
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./math_ops.db")
-LOG_FILE = os.getenv("LOG_FILE", "logs/app.log")
-CACHE_TTL = int(os.getenv("CACHE_TTL", "60"))
--- PythonMicroserviceApp/PythonMicroserviceProject/App/logger/logger.py
+++
@@ -1,5 +0,0 @@
-from loguru import logger
-from App.config import LOG_FILE
-
-def setup_logging():
-    logger.add(LOG_FILE, rotation="1 MB", retention="7 days", level="INFO")
